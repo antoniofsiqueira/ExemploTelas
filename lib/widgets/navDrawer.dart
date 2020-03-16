6ff3f1ed4cf421098c282d84e25848149b5d9abd
Antonio Francisco Alves Siqueira <antonio.fsiqueira@gmail.com>
2020-03-16 16:09:44 -0300

    Icons Side Bar

--- lib/widgets/navDrawer.dart
+++ lib/widgets/navDrawer.dart
@@ -7,45 +7,75 @@ class NavDrawer extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          DrawerHeader(
-            child: Text(
-              'Side menu',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+          Container(
+            height: 100.0,
+            child: DrawerHeader(
+              child: Text(
+                'APROFEM',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              
+              decoration: BoxDecoration(
+                
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/cover.jpg'))),
             ),
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/cover.jpg'))),
           ),
           ListTile(
-            leading: Icon(Icons.developer_board),
-            title: Text('Tela Notícias 1'),
+            leading: Icon(Icons.account_box),
+            title: Text('Carteirinha'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
             leading: Icon(Icons.developer_board),
-            title: Text('Tela Notícias 2'),
+            title: Text('Notícias'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: Icon(Icons.developer_board),
-            title: Text('Tela Notícias 3'),
+            leading: Icon(Icons.add_alert),
+            title: Text('Notificações'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
+            leading: Icon(Icons.airline_seat_recline_normal ),
+            title: Text('Publicaçoes do Servidor'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('Feedback'),
+            leading: Icon(Icons.art_track),
+            title: Text('Publicações do DOC'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.cake),
+            title: Text('Benefícios APROFEM'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_today),
+            title: Text('Agenda'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.assignment_late ),
+            title: Text('Direitos e Benefícios'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.link),
+            title: Text('Links Úteis'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.person_outline),
+            title: Text('Fale Conosco'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
             leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
+            title: Text('Sair'),
             onTap: () => {Navigator.of(context).pop()},
           ),
         ],
