bcdcc4c48e74d7df7efa29f6f43040c7920e0a2b
Antonio Francisco Alves Siqueira <antonio.fsiqueira@gmail.com>
2020-03-16 15:25:00 -0300

    SideBar Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teste/widgets/navDrawer.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: AppBar(
         title: Text(widget.title),
         flexibleSpace: Container(
@@ -47,74 +49,85 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: Center(
-              child: ListView(
-            children: const <Widget>[
-              Card(
-                child: ListTile(title: Text('Apenas o título')),
-                elevation: 10,
+        child: ListView(
+          children: const <Widget>[
+            Card(
+              child: ListTile(title: Text('Apenas o título')),
+              elevation: 10,
+            ),
+            Card(
+              elevation: 6,
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('Um título com uma imagem'),
               ),
-              Card(
-                elevation: 6,
-                child: ListTile(
-                  leading: FlutterLogo(),
-                  title: Text('Um título com uma imagem'),
+            ),
+            Card(
+              child: ListTile(
+                  title: Text('Apenas título e com data'),
+                  trailing: Text(
+                    "10/02/2020",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(fontSize: 10),
+                  )),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Apenas título e com data'),
+                trailing: Text(
+                  "10/02/2020",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 8),
+                ),
+                subtitle: Text(
+                  "Segue aqui uma breve descrição do subtitulo da notícia",
+                  style: TextStyle(fontSize: 10),
                 ),
               ),
-              Card(
-                child: ListTile(
-                    title: Text('Apenas título e icone'),
-                    trailing: Text(
-                      "10/02/2020",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(fontSize: 10),
-                    )),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('Imagem com título e icone'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                selected: true,
               ),
-              Card(
-                child: ListTile(
-                  leading: FlutterLogo(),
-                  title: Text('Imagem com título e icone'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  selected: true,
-                ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('Imagem com título e icone'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                selected: false,
               ),
-              Card(
-                child: ListTile(
-                  leading: FlutterLogo(),
-                  title: Text('Imagem com título e icone'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  selected: false,
-                ),
+            ),
+            Card(
+              elevation: 2,
+              child: ListTile(
+                title: Text('Título com dense true'),
+                dense: true,
               ),
-              Card(
-                elevation: 2,
-                child: ListTile(
-                  title: Text('Título com dense true'),
-                  dense: true,
-                ),
+            ),
+            Card(
+              elevation: 7,
+              child: ListTile(
+                leading: FlutterLogo(size: 56.0),
+                title: Text('Título da notícia'),
+                subtitle: Text('Subtitulo da notcia'),
+                trailing: Icon(Icons.more_vert),
               ),
-              Card(
-                elevation: 7,
-                child: ListTile(
-                  leading: FlutterLogo(size: 56.0),
-                  title: Text('Título da notícia'),
-                  subtitle: Text('Subtitulo da notcia'),
-                  trailing: Icon(Icons.more_vert),
-                ),
+            ),
+            Card(
+              elevation: 5,
+              child: ListTile(
+                title: Text('Titutlo do Notícia'),
+                subtitle:
+                    Text('Teste de texto simples para preeencher o card.'),
+                trailing: Text("link"),
+                isThreeLine: true,
               ),
-              Card(
-                elevation: 5,
-                child: ListTile(
-                  title: Text('Titutlo do Notícia'),
-                  subtitle:
-                      Text('Teste de texto simples para preeencher o card.'),
-                  trailing: Text("link"),
-                  isThreeLine: true,
-                ),
-              ),
-            ],
-          )),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
